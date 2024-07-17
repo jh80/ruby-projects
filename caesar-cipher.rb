d7f44fb0bb6659182f2262da7d2d2ea3ae0db8a7
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-07-17 11:21:24 -0700

    Replace reducing shiftNum with while loop
    Instead of doing work upfront to get the shiftNum
    below 26, I put in a while loops to reduce the ascii
    number to the appropriate range.

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,12 +1,6 @@
 
 def caesar_cipher(string, shiftNum)
    msg = ""
-   # Reduce shiftNum if is it larter than 26 or keep string if shiftNum is 26
-   if shiftNum == 26
-    return string
-   elsif shiftNum > 26
-    shiftNum = shiftNum%26
-   end
 
   # Check if char is a letter
   string.each_byte do |byte|
@@ -15,14 +9,14 @@ def caesar_cipher(string, shiftNum)
     if byte >= 65 && byte <= 90
       ascii += shiftNum
       # Loop back to "A"
-      if ascii > 90
+      while ascii > 90
         ascii -= 26
       end
     # Adjust if lowercase
     elsif byte >= 97 && byte <= 122
       ascii += shiftNum
       # Loop back to "a"
-      if ascii > 122
+      while ascii > 122
         ascii -= 26
       end
     end
