ca9a2c1e16a7c136700deae7019a3a865f5bc595
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-07-19 10:53:58 -0700

    Save and commit changes forgotten in last commit

--- substrings.rb
+++ substrings.rb
@@ -1,32 +1,31 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings (string, dictionary)
-  dictionary.each do |word|
-    matches = 0
-    for i in 0..(word.length - 1)
-      for j in 0..(string.length - 1)
-        loop do
-        if word[i + matches] == string[j + matches]
-          matches += 1
-        else
-          matches = 0
-        end
-        break if matches = 0
-      end
-    end
-  end
-end
-
-def substrings (string, dictionary)
+  hash = {}
+  string = string.downcase
   dictionary.each do |word|
     matches = 0
     for i in 0..(string.length - 1)
-      if string[i] == word[matches]
+      if string[i].downcase == word[matches]
+        if matches == 0
+          resume = i + 1
+        end
         matches += 1
         if matches == word.length
           # add word count to hash
+          if hash[word] 
+            hash[word] += 1
+          else
+            hash[word] = 1
+          end
+          # resume checking
+          i = resume
+          matches = 0
         end
+      else
+        matches = 0
       end
     end
   end
+  hash
 end
