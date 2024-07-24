41a14474efde3b285314c853567e7436f5c1813a
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-07-24 13:21:00 -0700

    Complete efficient and non-destructive bubble_sort
    This method copies the array passed into it in a
    non-destructive way, and utilizes the fact that
    after n passes the last n numbers are gaurenteed
    in order and don't need to be checked again.

--- bubble_sort2.rb
+++ bubble_sort2.rb
@@ -1,33 +1,21 @@
 def bubble_sort(array)
-  n = array.length
+  n = array.length-1
   sortedArray = []
-  swapped = false
-  array.each_with_index do |num, i|
-    if array [i + 1] == nil
-      break
-    end
-    if num > array[i + 1]
-      sortedArray[i] = array[i + 1]
-      sortedArray[i + 1] = array [i]
-      swapped = true
-    else 
-      sortedArray[i] = array [i]
-      sortedArray[i + 1] = array [i + 1]
-    end
-  end
-  if swapped == true
-    n = n - 2
+  swapped = true
+  array.each do |num|
+    sortedArray.push(num)
   end
   while swapped == true
     swapped = false
-    for i in 0...n
-      if sortedArray[i] > sortedArray[i + 1]
+    for i in 1..n
+      if sortedArray[i] < sortedArray[i - 1]
         temp = sortedArray[i]
-        sortedArray[i] = sortedArray[i + 1]
-        sortedArray[i + 1] = temp
+        sortedArray[i] = sortedArray[i - 1]
+        sortedArray[i - 1] = temp
         swapped = true
-        n = n-1
       end
+      n = n-1
+      p sortedArray
     end
   end
   sortedArray
