959523e67ca70a41e49e6a3aeaa097d0365bb6b7
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-07-24 14:24:35 -0700

    Add noteForSelf on a different way to write the parallel assignemnt

--- bubble_sort2.rb
+++ bubble_sort2.rb
@@ -9,6 +9,7 @@ def bubble_sort(array)
     swapped = false
     for i in 1..n
       if sortedArray[i] < sortedArray[i - 1]
+        # One of the other students (JohnDevHelix) wrote their parallel assignment like: array[(i-1)..i] = array[(i-1)..i].reverse   which I thought was cool 
         sortedArray[i], sortedArray[i - 1] = sortedArray[i - 1],sortedArray[i]
         swapped = true
       end
