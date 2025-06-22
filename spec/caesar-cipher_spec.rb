99dc662a7862eddded82ddb28ffb0b764b3c680c
Jennifer H <74622778+jh80@users.noreply.github.com>
2025-06-22 11:48:54 -0700

    Work some context in to test description

--- spec/caesar-cipher_spec.rb
+++ spec/caesar-cipher_spec.rb
@@ -5,39 +5,40 @@ require_relative '../caesar-cipher.rb'
 describe '.caesar-cipher' do
   context 'when message is one word' do
     context 'when message has lower-case, upper-case, and symbols' do
+      let(:message) { 'HeyHo!'}
 
-      context 'when shiftNum is under 26' do
-              
+      it 'works with shitfNum under 26' do
+        let
       end    
 
-      context 'when shiftNum is over 26' do
+      it 'works with shitfNum over 26' do
         
       end
 
-      context "when shiftNum is negative, but not under 26" do
+      it "works with negative shitfNum, but not under -26" do
         
       end
 
-      context "when shiftNum is negative under 26" do
+      it "works with shitfNum under -26" do
         
       end
     end
   end
 
-  context 'when message is more than one word' do
-    context 'when message is less than 26' do
+  context 'when message is phrase with puctuation' do
+    it 'works with shiftNum less than 26' do
       
     end
 
-    context 'when message is more than 26' do
+    it 'works with shitfNum more than 26' do
       
     end
 
-    context 'when message is negative but not less than -26' do
+    it 'works with negative shitfNum but not less than -26' do
       
     end
 
-    context 'when message in less -26' do
+    it 'works with shitfNum less -26' do
       
     end
   end
